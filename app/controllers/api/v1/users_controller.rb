38f32ad3a768463a303ce32d976f923a9d069a16
Bria222 <techconsultants.bree@gmail.com>
2022-12-25 23:14:17 +0300

    fix rubocop errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -47,6 +47,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :country_code, :email, :first_name, :last_name, :number, :password, :password_confirmation, :username)
+    params.require(:user).permit(:id, :country_code, :email, :first_name, :last_name, :number, :password,
+                                 :password_confirmation, :username)
   end
 end
