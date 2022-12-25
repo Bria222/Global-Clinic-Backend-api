8c32ec48307f519b3c519d60467e9a91413b0af8
Bria222 <techconsultants.bree@gmail.com>
2022-12-25 22:56:10 +0300

    create user params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -43,4 +43,10 @@ class Api::V1::UsersController < ApplicationController
       render json: 'unable to delete the user!', status: unprocessable_entity
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :country_code, :email, :first_name, :last_name, :number, :password, :password_confirmation, :username)
+  end
 end
