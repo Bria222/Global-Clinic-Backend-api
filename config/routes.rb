73001186fd9f3048b4c0d174a2b96b480565c9b8
Bria222 <techconsultants.bree@gmail.com>
2022-12-25 22:44:32 +0300

    generate users controller with five actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
